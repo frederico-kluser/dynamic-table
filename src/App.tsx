944bf18472657f615540c831c4e127f979f901fe
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-12-22 19:39:07 -0300

    feat: change theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,32 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import { Button } from '@mui/material';
+import { Button, createTheme, CssBaseline, PaletteMode, ThemeProvider, useMediaQuery } from '@mui/material';
+import { useMemo, useState } from 'react';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [mode, setMode] = useState<PaletteMode>(prefersDarkMode? 'dark' : 'light');
+
+  const handleClick = () => setMode(mode === 'dark'? 'light' : 'dark');
+
+  console.log('mode :', mode);
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode, prefersDarkMode],
+  );
+
   return (
-    <Button variant="contained">Hello World</Button>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Button variant="contained" onClick={handleClick}>Hello World</Button>
+    </ThemeProvider>
   )
 }
 

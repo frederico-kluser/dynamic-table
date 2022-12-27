1e724d2097d110cea3fec9227622b08de8b42e62
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-12-27 11:19:33 -0300

    fix: row size

--- src/component/Table/TableHead/index.tsx
+++ src/component/Table/TableHead/index.tsx
@@ -11,43 +11,54 @@ const EnhancedTableHead = (props: EnhancedTableProps) => {
       onRequestSort(event, property);
     };
 
+  const cols = [];
+  const length = Object.keys(data[0]).length + 1;
+  for (let i = 0; i < length; i++) {
+    cols.push(<col width={`${100/length}%`} />);
+  }
+
   return (
-    <TableHead>
-      <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < data.length}
-            checked={data.length > 0 && numSelected === data.length}
-            onChange={onSelectAllClick}
-            inputProps={{
-              'aria-label': 'select all desserts',
-            }}
-          />
-        </TableCell>
-        {makeHeadCells(data).map((headCell: any) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? 'right' : 'left'}
-            padding={headCell.disablePadding ? 'none' : 'normal'}
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </Box>
-              ) : null}
-            </TableSortLabel>
+    <>
+      <colgroup>
+        {cols}
+      </colgroup>
+      <TableHead>
+        <TableRow>
+          <TableCell padding="checkbox">
+            <Checkbox
+              color="primary"
+              indeterminate={numSelected > 0 && numSelected < data.length}
+              checked={data.length > 0 && numSelected === data.length}
+              onChange={onSelectAllClick}
+              inputProps={{
+                'aria-label': 'select all desserts',
+              }}
+            />
           </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
+          {makeHeadCells(data).map((headCell: any) => (
+            <TableCell
+              key={headCell.id}
+              align={headCell.numeric ? 'right' : 'left'}
+              padding={headCell.disablePadding ? 'none' : 'normal'}
+              sortDirection={orderBy === headCell.id ? order : false}
+            >
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <Box component="span" sx={visuallyHidden}>
+                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  </Box>
+                ) : null}
+              </TableSortLabel>
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    </>
   );
 }
 
